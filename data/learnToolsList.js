8940e89d811f9594a32ce2b469b5d1c12f044252
msuudoct2@gmail.com <msuudoct2@gmail.com>
2025-08-28 16:42:15 +0530

    Revert "Fixed tools page"
    This reverts commit 32fd097b6ef2c69a8e7e51dae22f0e60f3d149a2.

--- data/learnToolsList.js
+++ data/learnToolsList.js
@@ -1,44 +1,44 @@
-import gitIcon from "../src/assets/tools_logo/icons8-git.svg";
-export const learnToolsList = [
-  {
-    name: "Visual Studio Code",
-    icon: "../src/assets/tools_logo/icons8-vscode.svg",
-  },
-  {
-    name: "Google Chrome DevTools",
-    icon: "../src/assets/tools_logo/icons8-chrome.svg",
-  },
-  {
-    name: "React.js",
-    icon: "../src/assets/tools_logo/icons8-react.svg",
-  },
-  {
-    name: "Node.js",
-    icon: "../src/assets/tools_logo/icons8-nodejs.svg",
-  },
-  {
-    name: "Git & GitHub",
-    icon: gitIcon,
-  },
-  {
-    name: "Docker",
-    icon: "../src/assets/tools_logo/icons8-docker.svg",
-  },
-  {
-    name: "Figma",
-    icon: "../src/assets/tools_logo/icons8-figma.svg",
-  },
-  {
-    name: "Postman",
-    icon: "../src/assets/tools_logo/icons8-postman.svg",
-  },
-  {
-    name: "Tailwind CSS",
-    icon: "../src/assets/tools_logo/icons8-tailwind.svg",
-  },
-  {
-    name: "Sass",
-    icon: "../src/assets/tools_logo/icons8-sass.svg",
-  },
+import gitIcon from '../src/assets/tools_logo/icons8-git.svg';
+const learnToolsList = [
+    {
+        name: "Visual Studio Code",
+        icon: "../src/assets/tools_logo/icons8-vscode.svg"
+    },
+    {
+        name: "Google Chrome DevTools",
+        icon: "../src/assets/tools_logo/icons8-chrome.svg"
+    },
+    {
+        name: "React.js",
+        icon: "../src/assets/tools_logo/icons8-react.svg"
+    },
+    {
+        name: "Node.js",
+        icon: "../src/assets/tools_logo/icons8-nodejs.svg"
+    },
+    {
+        name: "Git & GitHub",
+        icon: gitIcon
+    },
+    {
+        name: "Docker",
+        icon: "../src/assets/tools_logo/icons8-docker.svg"
+    },
+    {
+        name: "Figma",
+        icon: "../src/assets/tools_logo/icons8-figma.svg"
+    },
+    {
+        name: "Postman",
+        icon: "../src/assets/tools_logo/icons8-postman.svg"
+    },
+    {
+        name: "Tailwind CSS",
+        icon: "../src/assets/tools_logo/icons8-tailwind.svg"
+    },
+    {
+        name: "Sass",
+        icon: "../src/assets/tools_logo/icons8-sass.svg"
+    }
 ];
 export default learnToolsList;
--- data/tutorialData.js
+++ data/tutorialData.js
@@ -4,380 +4,380 @@ export const tutorialData = {
     tools: [
       {
         name: "Visual Studio Code",
-        logo: "/src/assets/tools_logo/icons8-vs-code.svg",
+        logo: "./src/assets/tools_logo/icons8-vs-code.svg",
         tutorials: [
           {
             title: "The ULTIMATE VS Code Setup - Extensions & Settings 2025",
-            link: "https://www.youtube.com/watch?v=lxRAj1Gijic",
+            link: "https://www.youtube.com/watch?v=lxRAj1Gijic"
           },
           {
             title: "VS Code Crash Course | Visual Studio Code 2025",
-            link: "https://www.youtube.com/watch?v=fSQMBZ089HY",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=fSQMBZ089HY"
+          }
+        ]
       },
       {
         name: "Jupyter Notebook",
-        logo: "/src/assets/tools_logo/jupyter-logo.png",
+        logo: "./src/assets/tools_logo/jupyter-logo.png",
         tutorials: [
           {
             title: "Jupyter Notebook Complete Beginner Guide",
-            link: "https://www.youtube.com/watch?v=HW29067qVWk",
+            link: "https://www.youtube.com/watch?v=HW29067qVWk"
           },
           {
             title: "Jupyter Notebook Tips and Tricks",
-            link: "https://www.youtube.com/watch?v=7wfPqAyYADY",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=7wfPqAyYADY"
+          }
+        ]
       },
       {
         name: "Anaconda",
-        logo: "/src/assets/tools_logo/anaconda-logo.png",
+        logo: "./src/assets/tools_logo/anaconda-logo.png",
         tutorials: [
           {
             title: "Anaconda Beginners Guide for Linux and Windows",
-            link: "https://www.youtube.com/watch?v=YJC6ldI3hWk",
+            link: "https://www.youtube.com/watch?v=YJC6ldI3hWk"
           },
           {
             title: "Python Anaconda Tutorial - Getting Started",
-            link: "https://www.youtube.com/watch?v=ovlID7gefzE",
-          },
-        ],
-      },
-    ],
+            link: "https://youtu.be/ovlID7gefzE?si=pBLUJt0LRJgh_On_"
+          }
+        ]
+      }
+    ]
   },
   "Browser & Debugging Tools": {
     tools: [
       {
         name: "Google Chrome DevTools",
-        logo: "/src/assets/tools_logo/icons8-chrome.svg",
+        logo: "./src/assets/tools_logo/icons8-chrome.svg",
         tutorials: [
           {
-            title: "Chrome Dev Tools 101: A Beginner's Guide",
-            link: "https://www.youtube.com/watch?v=H0XScE08hy8",
+            title: "Chrome Dev Tools 101: A Beginner's Guide to Using Dev Tools",
+            link: "https://www.youtube.com/watch?v=fxplz32rgEQ"
           },
           {
             title: "How to use Chrome DevTools like a pro? | Part 1",
-            link: "https://www.youtube.com/watch?v=fgbqkVOH7Q4",
-          },
-        ],
-      },
-    ],
+            link: "https://www.youtube.com/watch?v=fgbqkVOH7Q4"
+          }
+        ]
+      }
+    ]
   },
   "Version Control & Collaboration": {
     tools: [
       {
         name: "Git & GitHub",
-        logo: "/src/assets/tools_logo/icons8-git.svg",
+        logo: "./src/assets/tools_logo/icons8-git.svg",
         tutorials: [
           {
             title: "Git and GitHub for Beginners - Crash Course",
-            link: "https://www.youtube.com/watch?v=RGOj5yH7evk",
+            link: "https://www.youtube.com/watch?v=RGOj5yH7evk"
           },
           {
             title: "Learn Git in 1 Hour - Full Beginners Tutorial",
-            link: "https://www.youtube.com/watch?v=8JJ101D3knE",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=8JJ101D3knE"
+          }
+        ]
       },
       {
         name: "Jira",
-        logo: "/src/assets/tools_logo/jira-logo.svg",
+        logo: "./src/assets/tools_logo/jira-logo.svg",
         tutorials: [
           {
-            title: "Jira Tutorial for Beginners (2025 tutorial)",
-            link: "https://www.youtube.com/watch?v=1RilyCsxp7U",
-          },
-        ],
+            title: "Jira Tutorial for Beginners: Jira Project Management",
+            link: "https://www.youtube.com/watch?v=7bdq7sh05UM"
+          }
+        ]
       },
       {
         name: "Trello",
-        logo: "/src/assets/tools_logo/trello-logo.jpg",
+        logo: "./src/assets/tools_logo/trello-logo.jpg",
         tutorials: [
           {
-            title: "Trello for Beginners 2025 (Everything You Need to Know)",
-            link: "https://www.youtube.com/watch?v=xky48zyL9iA",
-          },
-        ],
+            title: "Trello Tutorial for Beginners - Project Management",
+            link: "https://www.youtube.com/watch?v=1rDk2b2QNL4"
+          }
+        ]
       },
       {
         name: "Notion",
-        logo: "/src/assets/tools_logo/notion-logo.png",
+        logo: "./src/assets/tools_logo/notion-logo.png",
         tutorials: [
           {
-            title: "Notion for Beginners",
-            link: "https://www.youtube.com/watch?v=GlSR9PI2vpM",
-          },
-        ],
+            title: "Notion – Full Tutorial for Beginners",
+            link: "https://www.youtube.com/watch?v=UJ3gE-EWZqM"
+          }
+        ]
       },
       {
         name: "Slack",
-        logo: "/src/assets/tools_logo/slack-logo.png",
+        logo: "./src/assets/tools_logo/slack-logo.png",
         tutorials: [
           {
-            title: "How to Use Slack - Full Tutorial for Beginners",
-            link: "https://www.youtube.com/watch?v=o3HJuPaITWk",
-          },
-        ],
-      },
-    ],
+            title: "Slack Tutorial for Beginners - How to Use Slack",
+            link: "https://www.youtube.com/watch?v=B6zVzWU35v0"
+          }
+        ]
+      }
+    ]
   },
   "Backend & DevOps": {
     tools: [
       {
         name: "Docker",
-        logo: "/src/assets/tools_logo/docker-logo.png",
+        logo: "./src/assets/tools_logo/docker-logo.png",
         tutorials: [
           {
-            title: "Docker Tutorial for Beginners - Complete one shot",
-            link: "https://www.youtube.com/watch?v=exmSJpJvIPs",
-          },
-        ],
+            title: "Docker Tutorial for Beginners - Full Course",
+            link: "https://www.youtube.com/watch?v=3c-iBn73dDE"
+          }
+        ]
       },
       {
         name: "Kubernetes",
-        logo: "/src/assets/tools_logo/kubernetes-logo.png",
+        logo: "./src/assets/tools_logo/kubernetes-logo.png",
         tutorials: [
           {
-            title: "Complete Kubernetes Tutorial for Beginners",
-            link: "https://www.youtube.com/watch?v=VnvRFRk_51k&list=PLy7NrYWoggjziYQIDorlXjTvvwweTYoNC",
-          },
-        ],
+            title: "Kubernetes Tutorial for Beginners - Full Course",
+            link: "https://www.youtube.com/watch?v=X48VuDVv0do"
+          }
+        ]
       },
       {
         name: "Kubernetes Lens",
-        logo: "/src/assets/tools_logo/lens-logo.png",
+        logo: "./src/assets/tools_logo/lens-logo.png",
         tutorials: [
           {
-            title: "Complete Lens Course - The Best Kubernetes IDE for DevOps",
-            link: "https://www.youtube.com/watch?v=VOruC2tQbmw",
-          },
-        ],
+            title: "Kubernetes Lens - Complete Tutorial",
+            link: "https://www.youtube.com/watch?v=5lXq5k2YbwM"
+          }
+        ]
       },
       {
         name: "NGINX",
-        logo: "/src/assets/tools_logo/nginx-logo.png",
+        logo: "./src/assets/tools_logo/nginx-logo.png",
         tutorials: [
           {
-            title: "NGINX Crash Course",
-            link: "https://www.youtube.com/watch?v=9t9Mp0BGnyI",
-          },
-        ],
+            title: "NGINX Tutorial for Beginners",
+            link: "https://www.youtube.com/watch?v=7VAI73roXaY"
+          }
+        ]
       },
       {
         name: "Render",
-        logo: "/src/assets/tools_logo/render-logo.jpg",
+        logo: "./src/assets/tools_logo/render-logo.jpg",
         tutorials: [
           {
-            title: "Deploy any application with one click using Render",
-            link: "https://www.youtube.com/watch?v=yWxBUcG_C7g",
-          },
-        ],
+            title: "Deploy to Render - Full Tutorial",
+            link: "https://www.youtube.com/watch?v=6Xxx5KnJYX4"
+          }
+        ]
       },
       {
         name: "Railway",
-        logo: "/src/assets/tools_logo/railway-logo.svg",
+        logo: "./src/assets/tools_logo/railway-logo.svg",
         tutorials: [
           {
             title: "Railway - Deploy Any App in Seconds",
-            link: "https://www.youtube.com/watch?v=7XUibDYw4mc",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=7XUibDYw4mc"
+          }
+        ]
       },
       {
         name: "Heroku",
-        logo: "/src/assets/tools_logo/heroku-logo.svg",
+        logo: "./src/assets/tools_logo/heroku-logo.svg",
         tutorials: [
           {
-            title: "Deploying a MERN Stack App to Heroku",
-            link: "https://www.youtube.com/watch?v=G0jO8kUrg-I",
-          },
-        ],
-      },
-    ],
+            title: "Deploy to Heroku - Step by Step",
+            link: "https://www.youtube.com/watch?v=G0jO8kUrg-I"
+          }
+        ]
+      }
+    ]
   },
   "APIs & Communication": {
     tools: [
       {
         name: "Twilio",
-        logo: "/src/assets/tools_logo/twilio-icon.svg",
+        logo: "./src/assets/tools_logo/twilio-icon.svg",
         tutorials: [
           {
             title: "Twilio complete beginners guide",
-            link: "https://www.youtube.com/watch?v=4jUMqutYmyE",
+            link: "https://www.youtube.com/watch?v=4jUMqutYmyE"
           },
           {
             title: "Twilio tutorial for beginners",
-            link: "https://www.youtube.com/watch?v=8OIl8pMK90U",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=8OIl8pMK90U"
+          }
+        ]
       },
       {
         name: "Postman",
-        logo: "/src/assets/tools_logo/postman-logo.png",
+        logo: "./src/assets/tools_logo/postman-logo.png",
         tutorials: [
           {
             title: "Postman Beginner's Course - API Testing",
-            link: "https://www.youtube.com/watch?v=VywxIQ2ZXw4",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=VywxIQ2ZXw4"
+          }
+        ]
       },
       {
         name: "OpenAI API",
-        logo: "/src/assets/tools_logo/openai-logo.png",
+        logo: "./src/assets/tools_logo/openai-logo.png",
         tutorials: [
           {
-            title: "OpenAI Api Crash Course For Beginners",
-            link: "https://www.youtube.com/watch?v=xP_ZON_P4Ks",
-          },
-        ],
+            title: "OpenAI API - Complete Guide",
+            link: "https://www.youtube.com/watch?v=oJjgu5GkHk8"
+          }
+        ]
       },
       {
         name: "Hugging Face",
-        logo: "/src/assets/tools_logo/huggingface-logo.png",
+        logo: "./src/assets/tools_logo/huggingface-logo.png",
         tutorials: [
           {
             title: "Hugging Face Course - Transformers",
-            link: "https://www.youtube.com/watch?v=7PhlevizVB4",
-          },
-        ],
-      },
-    ],
+            link: "https://www.youtube.com/watch?v=7PhlevizVB4"
+          }
+        ]
+      }
+    ]
   },
   "Databases & ORMs": {
     tools: [
       {
         name: "Firebase",
-        logo: "/src/assets/tools_logo/firebase-1-logo.svg",
+        logo: "./src/assets/tools_logo/firebase-1-logo.svg",
         tutorials: [
           {
-            title: "Firebase Full Course for Beginners",
-            link: "https://www.youtube.com/watch?v=fgdpvwEWJ9M",
+            title: "Firebase basics tutorial",
+            link: "https://www.youtube.com/watch?v=9kRgVxULbag"
           },
           {
-            title: "Firebase Tutorial 2025",
-            link: "https://www.youtube.com/watch?v=_L8j-ZC83y4",
-          },
-        ],
+            title: "Firebase tutorial 2025",
+            link: "https://www.youtube.com/watch?v=_L8j-ZC83y4"
+          }
+        ]
       },
       {
         name: "Supabase",
-        logo: "/src/assets/tools_logo/supabase-logo.png",
+        logo: "./src/assets/tools_logo/supabase-logo.png",
         tutorials: [
           {
-            title: "Supabase Crash Course 2025",
-            link: "https://www.youtube.com/watch?v=ydz7Dj5QHKY&list=PL4cUxeGkcC9hUb6sHthUEwG7r9VDPBMKO",
-          },
-        ],
+            title: "Supabase Crash Course",
+            link: "https://www.youtube.com/watch?v=WiwfiVdfRIc"
+          }
+        ]
       },
       {
         name: "Prisma",
-        logo: "/src/assets/tools_logo/prisma-logo.svg",
+        logo: "./src/assets/tools_logo/prisma-logo.svg",
         tutorials: [
           {
             title: "Prisma Crash Course",
-            link: "https://www.youtube.com/watch?v=CYH04BJzamo",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=Reb7KS-js9Q"
+          }
+        ]
       },
       {
         name: "Sequelize",
-        logo: "/src/assets/tools_logo/sequelize-logo.png",
+        logo: "./src/assets/tools_logo/sequelize-logo.png",
         tutorials: [
           {
             title: "Sequelize ORM - Full Course",
-            link: "https://www.youtube.com/watch?v=dPM3POQjyqo&list=PLsvvBhdpMqBzyrmkAAO5zsOwO0wCTU7bj",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=pxo7LrUaZ4M"
+          }
+        ]
       },
       {
         name: "ElasticSearch",
-        logo: "/src/assets/tools_logo/elasticsearch-logo.png",
+        logo: "./src/assets/tools_logo/elasticsearch-logo.png",
         tutorials: [
           {
             title: "ElasticSearch Tutorial",
-            link: "https://www.youtube.com/watch?v=bcsII2dGnDU&list=PLA3GkZPtsafYd5m2BXmkL9pjsBKy0FQ2X",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=1C7V9B0fQqI"
+          }
+        ]
       },
       {
         name: "Kibana",
-        logo: "/src/assets/tools_logo/kibana-logo.png",
+        logo: "./src/assets/tools_logo/kibana-logo.png",
         tutorials: [
           {
             title: "Kibana Tutorial for Beginners",
-            link: "https://www.youtube.com/watch?v=l2b04Sz8vTw&list=PLhLSfisesZItoCPKxNHhttGkCTgCrAW2I",
-          },
-        ],
-      },
-    ],
+            link: "https://www.youtube.com/watch?v=ZP0Nmfy-uoY"
+          }
+        ]
+      }
+    ]
   },
   "Frontend & Styling": {
     tools: [
       {
         name: "Tailwind CSS",
-        logo: "/src/assets/tools_logo/tailwindcss-logo.png",
+        logo: "./src/assets/tools_logo/tailwindcss-logo.png",
         tutorials: [
           {
-            title: "Tailwind CSS Full Course for Beginners",
-            link: "https://www.youtube.com/watch?v=lCxcTsOHrjo",
-          },
-        ],
+            title: "Tailwind CSS Crash Course",
+            link: "https://www.youtube.com/watch?v=4wGmyl65gww"
+          }
+        ]
       },
       {
         name: "Sass",
-        logo: "/src/assets/tools_logo/sass-logo.png",
+        logo: "./src/assets/tools_logo/sass-logo.png",
         tutorials: [
           {
             title: "Sass Tutorial for Beginners",
-            link: "https://www.youtube.com/watch?v=_a5j7KoflTs",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=_a5j7KoflTs"
+          }
+        ]
       },
       {
         name: "Figma",
-        logo: "/src/assets/tools_logo/figma-logo.avif",
+        logo: "./src/assets/tools_logo/figma-logo.avif",
         tutorials: [
           {
             title: "Figma Tutorial: A Crash Course for Beginners",
-            link: "https://www.youtube.com/watch?v=3q3FV65ZrUs",
-          },
-        ],
+            link: "https://www.youtube.com/watch?v=3q3FV65ZrUs"
+          }
+        ]
       },
       {
         name: "Vercel",
-        logo: "/src/assets/tools_logo/vercel-logo.png",
+        logo: "./src/assets/tools_logo/vercel-logo.png",
         tutorials: [
           {
-            title: "How to Deploy to Vercel (2024)",
-            link: "https://www.youtube.com/watch?v=E8xaV6fiTaA",
-          },
-        ],
+            title: "Deploy to Vercel - Complete Guide",
+            link: "https://www.youtube.com/watch?v=QOYpHHxYCRo"
+          }
+        ]
       },
       {
         name: "Netlify",
-        logo: "/src/assets/tools_logo/netlify-logo.svg",
+        logo: "./src/assets/tools_logo/netlify-logo.svg",
         tutorials: [
           {
             title: "Deploy to Netlify - Full Tutorial",
-            link: "https://www.youtube.com/watch?v=9srnyNC1e_o",
-          },
-        ],
-      },
-    ],
+            link: "https://www.youtube.com/watch?v=BPgE4hTacqw"
+          }
+        ]
+      }
+    ]
   },
   "Data Science & Analytics": {
     tools: [
       {
         name: "Kaggle",
-        logo: "/src/assets/tools_logo/kaggle-logo.png",
+        logo: "./src/assets/tools_logo/kaggle-logo.png",
         tutorials: [
           {
             title: "Kaggle Tutorial for Beginners",
-            link: "https://www.youtube.com/watch?v=K6GSdn_iv8s",
-          },
-        ],
-      },
-    ],
-  },
+            link: "https://youtu.be/K6GSdn_iv8s?si=WKamu63TqRo5uHTI"
+          }
+        ]
+      }
+    ]
+  }
 };
